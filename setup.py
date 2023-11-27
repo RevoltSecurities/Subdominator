a01a408e17b2e434b20798270c7aa21fae09e7be
Sanjai kumar <119435129+sanjai-AK47@users.noreply.github.com>
2023-11-27 11:29:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='subdominator',
-    version='1.0.4',
+    version='1.0.5',
     author='D. Sanjai Kumar',
     author_email='bughunterz0047@gmail.com',
     description='Subdominator - An ultimate subdomain enumeration tool',
