82b4be132f5f68ae00c21f9b74bac109dbf9c0ab
RevoltSecurities <119435129+RevoltSecurities@users.noreply.github.com>
2024-07-22 18:05:38 +0530

    Delete subdominator directory

--- subdominator/modules/username/username.py
+++
@@ -1,9 +0,0 @@
-import os
-def get_username():
-    try:
-        username = os.getlogin()
-    except OSError:
-        username = os.getenv('USER') or os.getenv('LOGNAME') or os.getenv('USERNAME') or 'Unknown User'
-    except Exception as e:
-        username = "Unknown User"    
-    return username
--- subdominator/subdominator.py
+++
@@ -1,7 +0,0 @@
-from subdominator.modules.handler import main_handler
-
-def main():
-    main_handler()
-    
-if __name__ == "__main__":
-    main()
