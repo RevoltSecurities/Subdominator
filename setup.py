b5e520901ca0d35721ec5ec07abba06bfe233833
RevoltSecurities <119435129+RevoltSecurities@users.noreply.github.com>
2024-10-10 20:17:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
-with open("README.md", "r") as streamr:
+with open("README.md", "r", encoding="utf-8") as streamr:
     long_description = streamr.read()
 
 setup(
     name='subdominator',
-    version='1.0.8',
+    version='1.0.9',
     author='D. Sanjai Kumar',
     author_email='bughunterz0047@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sanjai-AK47/Subdominator",
+    url="https://github.com/RevoltSecurities/Subdominator",
     description='Subdominator - An ultimate subdomain enumeration tool for Security Researchers and Bug Bounty Hunters',
     packages=find_packages(),
     install_requires=[
@@ -17,6 +17,7 @@ setup(
         'aiohttp>=3.9.4',
         'appdirs>=1.4.4',
         'art>=6.1',
+        'httpx>=0.27.2',
         'fake_useragent>=1.5.0',
         'beautifulsoup4>=4.11.1',
         'PyYAML>=6.0.1',
