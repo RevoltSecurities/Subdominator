373b72226b86db0898fec332811040c5e4bae28b
RevoltSecurities <119435129+RevoltSecurities@users.noreply.github.com>
2024-10-10 20:18:31 +0530

    Delete subdominator/modules directory

--- subdominator/modules/extract/extract.py
+++
@@ -1,8 +0,0 @@
-def filters(results):
-    filtered = []
-    for subdomains in results:
-        if subdomains is None:
-            continue
-        for subdomain in subdomains:
-            filtered.append(subdomain)
-    return sorted(set(filtered))
--- subdominator/modules/verify/verify.py
+++
@@ -1,4 +0,0 @@
-import importlib.metadata as data
-def getverify(pkg):
-    version = data.version(pkg)
-    return version
