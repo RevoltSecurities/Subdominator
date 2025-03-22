f2f9b1dc1cd4673a5437dc641e907d14043bbc76
RevoltSecurities <119435129+RevoltSecurities@users.noreply.github.com>
2025-03-22 12:20:38 +0530

    Delete subdominator directory

--- subdominator/modules/extract/extract.py
+++
@@ -1,9 +0,0 @@
-def filters(results):
-    filtered = []
-    for subdomains in results:
-        if subdomains is None:
-            subdomains = []
-        for subdomain in subdomains:
-            if subdomain is not None:
-                filtered.append(subdomain)
-    return sorted(set(filtered))
